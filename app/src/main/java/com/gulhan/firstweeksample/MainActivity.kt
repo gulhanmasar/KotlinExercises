ed8244e25bdccacac4e13339f5e357c82953796c
gulhan <msrglhn@gmail.com>
2022-08-25 15:12:34 +0300

    [gulhanmasar] first week sample created button changed text

---
+++ app/src/main/java/com/gulhan/firstweeksample/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.gulhan.firstweeksample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var button: Button
+    private lateinit var myText: TextView
+    private lateinit var imageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+     button = findViewById(R.id.button)
+     myText = findViewById(R.id.textView2)
+     button.setOnClickListener{
+        // Toast.makeText(this, "Button clicked." , Toast.LENGTH_SHORT).show()
+      myText.text = "Text changed !"
+      imageView = findViewById(R.id.imageView2)
+
+     }
+
+    }
+}
